fbde1be3638c4bc2d45f80dd8bc90b48275e7c1c
Alberto del Olmo <alberto.delolmo@cuatroochenta.com>
2026-05-27 12:34:09 +0200

    Use frontend generated UUID for products

--- backend/app/src/Controller/ProductController.php
+++ backend/app/src/Controller/ProductController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class ProductController
 {
@@ -30,7 +31,9 @@ final class ProductController
     }
 
     #[Route('/api/products', name: 'api_products_create', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $entityManager): JsonResponse {
+    public function create(Request $request, EntityManagerInterface $entityManager,
+        ValidatorInterface $validator, ProductRepository $productRepository
+    ): JsonResponse {
 
         $payload = json_decode($request->getContent(), true);
 
@@ -40,17 +43,34 @@ final class ProductController
             ], JsonResponse::HTTP_BAD_REQUEST);
         }
 
-        if (empty($payload['name']) || empty($payload['measurementUnit']) || empty($payload['actualStock'])) {
-            return new JsonResponse([
-                'error' => 'Nombre, Unidad de medida y stock actual son obligatorios',
-            ], JsonResponse::HTTP_BAD_REQUEST);
-        }
-
         $product = new Product();
+        $product -> setId($payload['id']);
         $product -> setName($payload['name']);
         $product -> setDescription($payload['description'] ?? null);
         $product -> setMeasurementUnit($payload['measurementUnit']);
-        $product -> setActualStock($payload['actualStock']);
+        $product -> setActualStock(isset($payload['actualStock']) ? (int) $payload['actualStock'] : null);
+
+        $errors = $validator->validate($product);
+
+        if(count($errors) > 0){
+            $errorMessages = [];
+
+            foreach ($errors as $error) {
+                $errorMessages[] = [
+                    'field' => $error->getPropertyPath(),
+                    'message' => $error->getMessage(),
+                ];
+            }
+            return new JsonResponse([
+                'errors' => $errorMessages,
+            ], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        if ($productRepository->find($product->getId()) !== null) {
+            return new JsonResponse([
+                'error' => 'Ya existe un producto con este ID',
+            ], JsonResponse::HTTP_CONFLICT);
+        }
 
         $entityManager -> persist($product);
         $entityManager->flush();
--- backend/app/src/Entity/Product.php
+++ backend/app/src/Entity/Product.php
@@ -5,30 +5,46 @@ namespace App\Entity;
 use App\Repository\ProductRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: Types::GUID, unique: true)]
+    #[Assert\NotBlank(message: 'El id del producto es obligatorio.')]
+    #[Assert\Uuid(message: 'El id del producto debe de tener formato UUID.')]
+    private ?string $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\notBlank(message: 'El nombre del producto es obligatorio.')]
+    #[Assert\Length(max: 255, maxMessage: "El nombre del producto no puede tener más de 255 carácteres.")]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Assert\Length(max: 1000, maxMessage: "La descripción del producto no puede tener más de 1000 carácteres.")]
     private ?string $description = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\notBlank(message: 'La unidad de medida del producto es obligatoria.')]
+    #[Assert\Length(max: 255, maxMessage: "La unidad de medida del producto no puede tener más de 50 carácteres.")]
     private ?string $measurementUnit = null;
 
     #[ORM\Column]
+    #[Assert\NotNull(message: 'El stock actual es obligatorio.')]
+    #[Assert\PositiveOrZero(message: 'El stock actual no puede ser negativo.')]
     private ?int $actualStock = null;
 
-    public function getId(): ?int
+    public function getId(): ?string
     {
         return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
     }
 
     public function getName(): ?string
@@ -72,7 +88,7 @@ class Product
         return $this->actualStock;
     }
 
-    public function setActualStock(int $actualStock): static
+    public function setActualStock(?int $actualStock): static
     {
         $this->actualStock = $actualStock;
 
